987d1d71105e3c8aee2a016b6e20f2fc69c57bd2
andrew <leegd120@gmail.com>
2026-01-11 21:57:39 +0900

    fix: 파일 location 수정

--- src/main/java/com/project/global/config/RedisConfig.java
+++ src/main/java/com/project/global/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.project.config;
+package com.project.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/project/global/config/SwaggerConfig.java
+++ src/main/java/com/project/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.project.config;
+package com.project.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
