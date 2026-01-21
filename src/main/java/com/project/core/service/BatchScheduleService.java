ee5312be581c7aaddae93afd6f09fa104754e18b
yechan <100present@naver.com>
2026-01-21 22:21:00 +0900

    UPLUS-121 feat : service, trigger 예외처리 완료

--- src/main/java/com/project/core/service/BatchScheduleService.java
+++ src/main/java/com/project/core/service/BatchScheduleService.java
@@ -1,16 +1,32 @@
 package com.project.core.service;
 
 import com.project.core.infra.repository.invoice.BatchScheduleRepository;
+import com.project.global.exception.code.domain.core.CoreErrorCode;
+import com.project.global.exception.core.OperationFailedException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BatchScheduleService {
 
     private final BatchScheduleRepository repository;
 
     public void updateSchedule(String jobName, String cron) {
-        repository.upsert(jobName, cron);
+
+        try{
+            repository.upsert(jobName, cron);
+        } catch (Exception e){
+            log.error(
+                    "[BatchSchedule] update failed. jobName={}, cron={}",
+                    jobName,
+                    cron,
+                    e
+            );
+
+            throw new OperationFailedException(CoreErrorCode.BATCH_SCHEDULE_UPDATE_FAILED);
+        }
     }
 }
--- src/main/java/com/project/core/util/BatchTriggerClient.java
+++ src/main/java/com/project/core/util/BatchTriggerClient.java
@@ -1,11 +1,15 @@
 package com.project.core.util;
 
 import com.project.global.config.CloudFunctionProperties;
+import com.project.global.exception.code.domain.core.CoreErrorCode;
+import com.project.global.exception.core.OperationFailedException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class BatchTriggerClient {
 
@@ -16,21 +20,49 @@ public class BatchTriggerClient {
 
         String url = properties.getBaseUrl() + "/run-now";
 
-        restTemplate.postForEntity(
-                url + "?jobName=" + jobName + "&invMonth=" + invMonth,
-                null,
-                Void.class
-        );
+        try {
+            restTemplate.postForEntity(
+                    url + "?jobName=" + jobName + "&invMonth=" + invMonth,
+                    null,
+                    Void.class
+            );
+
+        } catch (Exception e) {
+            log.error(
+                    "[BatchTrigger] run-now failed. jobName={}, invMonth={}",
+                    jobName,
+                    invMonth,
+                    e
+            );
+
+            throw new OperationFailedException(
+                    CoreErrorCode.BATCH_TRIGGER_FAILED
+            );
+        }
     }
 
-    public void triggerSchedule(String jobName, String cron){
+    public void triggerSchedule(String jobName, String cron) {
 
         String url = properties.getBaseUrl() + "/schedule";
 
-        restTemplate.postForEntity(
-                url + "?jobName=" + jobName + "&cron=" + cron,
-                null,
-                Void.class
-        );
+        try {
+            restTemplate.postForEntity(
+                    url + "?jobName=" + jobName + "&cron=" + cron,
+                    null,
+                    Void.class
+            );
+
+        } catch (Exception e) {
+            log.error(
+                    "[BatchTrigger] schedule trigger failed. jobName={}, cron={}",
+                    jobName,
+                    cron,
+                    e
+            );
+
+            throw new OperationFailedException(
+                    CoreErrorCode.BATCH_TRIGGER_FAILED
+            );
+        }
     }
 }
