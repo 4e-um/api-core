e88078a95bfb41e85eeb57af385bfdefab9c7826
yechan <100present@naver.com>
2026-01-23 16:32:14 +0900

    UPLUS-121 feat: RestConfig 최종 수정..

--- src/main/java/com/project/global/config/RestTemplateConfig.java
+++ src/main/java/com/project/global/config/RestTemplateConfig.java
@@ -1,8 +1,11 @@
 package com.project.global.config;
 
+import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,18 +19,35 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
 
+        // 1️⃣ TCP 연결 타임아웃
+        ConnectionConfig connectionConfig =
+                ConnectionConfig.custom()
+                        .setConnectTimeout(Timeout.ofSeconds(5))
+                        .build();
+
+        // 2️⃣ 요청/응답 타임아웃
         RequestConfig requestConfig =
                 RequestConfig.custom()
-                        .setConnectTimeout(Timeout.ofSeconds(5))
                         .setResponseTimeout(Timeout.ofSeconds(5))
+                        .setConnectionRequestTimeout(Timeout.ofSeconds(5))
+                        .build();
+
+        PoolingHttpClientConnectionManager cm =
+                PoolingHttpClientConnectionManagerBuilder.create()
+                        .setDefaultConnectionConfig(connectionConfig)
                         .build();
 
         CloseableHttpClient httpClient =
-                HttpClients.custom().setDefaultRequestConfig(requestConfig).build();
+                HttpClients.custom()
+                        .setConnectionManager(cm)
+                        .setDefaultRequestConfig(requestConfig)
+                        .build();
 
         HttpComponentsClientHttpRequestFactory factory =
                 new HttpComponentsClientHttpRequestFactory(httpClient);
 
-        return builder.requestFactory(() -> factory).build();
+        return builder
+                .requestFactory(() -> factory)
+                .build();
     }
 }
